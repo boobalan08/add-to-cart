ead75ff1af92f94cdf662c2a1029221e661621dc
boobalan08 <boobalans42@gmail.com>
2022-11-19 16:45:37 +0530

    new

---
+++ src/Components/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Cart.css";
+
+const NavBar = ({ cartCount }) => {
+  return (
+    <div className="navContainer">
+      <h2>Shopify</h2>
+      <ul>
+        <li>Home</li>
+        <li>Contact Us</li>
+        <li>About Us</li>
+      </ul>
+      {/* <div className="cart">
+      <img src="https://cdn-icons-png.flaticon.com/512/34/34627.png" className="cart_img"/><h4>Cart</h4><span className="cartNo">{cartCount}</span>
+      </div> */}
+      <button className="btn1">   <img src="https://cdn-icons-png.flaticon.com/512/34/34627.png" className="cart_img"/><h4>Cart</h4><span className="cartNo">{cartCount}</span> </button>
+    </div>
+  );
+};
+export default NavBar;
